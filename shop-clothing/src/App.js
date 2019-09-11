7081b7ad03f30fc56c7a8f5a5676bcd82a5a82af
cwp468 <cwp468@motorolasolutions.com>
2019-09-11 20:19:58 +0200

    routers

--- shop-clothing/src/App.js
+++ shop-clothing/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatsPage = (props) => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
